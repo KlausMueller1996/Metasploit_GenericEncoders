d4281eb28d395791183d0208a6436ef7c422ebd5
Klaus Mueller <klaumueller1969@gmail.com>
2022-06-23 11:05:27 +0200

    added naturalword and shuffle encoder

---
+++ naturalword.rb
@@ -0,0 +1,53 @@
+##
+# This module requires Metasploit: https://metasploit.com/download
+# Current source: https://github.com/rapid7/metasploit-framework
+##
+
+
+class MetasploitModule < Msf::Encoder
+
+## TODO: Limit file format to Framework Transform Formats
+
+  def initialize
+    super(
+      'Name'             => 'Natural Word Encoder',
+      'Description'      => %q{
+        This encoder use a translation table of 256 natural language words of 4 character size. Each byte value of the payload will be translated into the corresponding word.  
+      },
+      'Author'           => 'Klaus Mueller',
+      'License'          => MSF_LICENSE,
+      'Arch'             => ARCH_ALL,
+      'EncoderType'      => Msf::Encoder::Type::Raw)
+  end
+  
+  def encode_block(state, block)
+	 translation_table = [
+	 "DILL","CASH","EMSE","FACT","DEWA","BERG","FESE","ÖFFI","GLAU","BEYS","ALBE","EWIG","GRÄT","BOHR","ÄSSE","DUCK",
+	 "GAUR","DOME","BLUT","DEKO","DACH","ÜBEL","FURZ","CALF","FRÜG","ANNO","EMIR","BUGS","DING","DALK","DERB","FEGT",
+	 "AREN","ÖDEM","GÄUS","AALE","DIMM","DELI","GETA","ALGE","BOLZ","BÜXE","DITS","ADRE","EKLE","AAST","BAHT","BÄRS",
+	 "ESST","ERBT","FEIN","ÜZES","CLOG","BOYS","EMDE","DOJO","FLUR","BÜRD","BOBS","GIEN","EIDE","BAUS","DARG","DEEZ",
+	 "BÖGT","ALSO","AALT","ÖLES","FLUG","DEOS","BOHN","EGGE","ÖLST","GAZI","BÄRE","COCA","FAMA","ÖSEN","AARE","FIRN",
+	 "FLOH","ASAR","AHMT","AAKS","CUPS","AURA","DÄNE","BEIM","FANS","GALA","GILT","GATE","ARGE","FINK","FACK","ELUL",
+	 "DURA","CARS","GINS","DIPP","CORD","ALAN","BRIE","ARTE","BOAS","BASH","BURG","GEIE","DOLM","DOTZ","BOIS","GIEP",
+	 "GRAN","BIKT","ERDE","FLAU","ACHS","FALT","DIES","BIER","GEHR","GÖRS","BUHL","FAXT","BIDA","FLÖZ","FENZ","ALFA",
+	 "EINT","ENGT","GEIG","AINU","ENKE","BEUT","CURE","FAXE","AHLE","BLAK","ALLA","AJAX","DAUN","ARME","BROM","GLÜH",
+	 "DÜST","CAPE","GRÄM","DATE","AZOL","DEVA","BÄBÄ","GEIT","AUFN","BARR","AAME","DUNE","ALIA","APPS","DUPE","FAKE",
+	 "BEST","FÖRE","DION","ETAT","BEIE","DÜNG","DODO","BLAS","FRÄS","ANOA","EULE","FASE","DOLS","BOHL","DUAL","FIPS",
+	 "GIBT","BITS","EMMY","EDLE","DENS","ÖHMS","BAFF","GERM","ARZT","CITY","GRAY","ÖDER","BEÖL","AULS","BANE","EBBT",
+	 "AMID","EXIL","BELS","GAUE","ALOI","DIEB","CLUB","AXEL","GEER","BADE","FEIL","EIRE","ESTE","BASS","AHAR","AKNE",
+	 "BABY","EISE","DIOL","ACTS","ANIL","GAUL","BÖTE","EFEU","ACTA","FELG","GAUZ","AHNS","BRAM","FUZO","ÄTZT","BIOM",
+	 "GAGT","CLIP","BIGA","GÜTE","BRÜH","COLT","BÜRO","ALLE","BUHS","BUDE","BELT","DONG","DORT","ESSE","DENK","ELKO",
+	 "EREN","FECK","ANOM","EIBE","BANI","DOSE","GEHE","AHMS","ANKT","FORA","FAHL","DIEL","AMYL","BÜSI","DICK","GAFF"]
+
+    encoded = ''
+
+	 block.unpack('C*').each do |chr|
+      i = chr.ord
+      word = translation_table[i]
+      encoded << word
+    end
+    return encoded
+  end
+end
+
+
---
+++ shuffle.rb
@@ -0,0 +1,60 @@
+##
+# This module requires Metasploit: https://metasploit.com/download
+# Current source: https://github.com/rapid7/metasploit-framework
+##
+
+
+class MetasploitModule < Msf::Encoder
+
+  def initialize
+    super(
+      'Name'             => 'Shuffle Encoder',
+      'Description'      => %q{
+        This encoder Shuffles the uneven bytes in the payload to avoid regex pattern matching algorithms. Byte 1 is swapped with byte 3, byte 5 is swapped with byte 7, etc 
+      },
+      'Author'           => 'Klaus Mueller',
+      'License'          => MSF_LICENSE,
+      'Arch'             => ARCH_ALL,
+      'EncoderType'      => Msf::Encoder::Type::Raw)
+  end
+
+  def encode_block(state, block)
+
+    encoded = ''   
+    encoder_len = 0
+
+    my_array = block.unpack('C*')
+
+    # as the encoder needs blocks of 4 bytes we need to make sure we stop at the proper position 
+    len = my_array.length() 
+    
+    # number of bytes that need to be encoded 
+    encoder_len = len - len % 4
+
+    my_array.each_with_index do |val,index|
+    
+      ## shuffle bytes if we have a complete block of 4 left
+      if index <= encoder_len
+        
+        # if are at position 1 we take byte number 3
+		  if index % 4 == 1
+          encoded << my_array[index+2].chr
+
+        # if are at position 3 we take byte number 1
+        elsif index % 4 == 3
+          encoded << my_array[index-2].chr
+
+        # the even bytes remain unchanged
+        else
+          encoded << val.chr
+        end
+
+      # append the last bytes as they are 
+      else
+        encoded << val.chr
+      end
+      
+    end    
+    return encoded
+  end
+end
